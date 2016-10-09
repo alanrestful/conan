a5ff0fdb59d352e796c41e3d4272b4d3eda89194
JSANN <zenwink@gmail.com>
2016-10-09 11:11:03 +0800

    modify group detail comp

--- extension/client/app/components/panel/group_detail/index.js
+++ extension/client/app/components/panel/group_detail/index.js
@@ -18,6 +18,7 @@ export default class extends React.Component {
     this.state = {
       models: [],
       selectedGroup: {},
+      selectedModel: {},
       createCaseModalVisible: false
     }
   }
@@ -53,13 +54,22 @@ export default class extends React.Component {
 
   deleteGroup() {}
 
-  changeGroupSelected() {}
+  selectedModel(model) {
+    this.setState({
+      selectedModel: model
+    });
+  }
+
+  checkedModel() {}
 
   confirm() {}
 
   render() {
     let models = this.state.models,
+        model = this.state.selectedModel,
+        fragment = isEmpty(model) ? [] : JSON.parse(model.fragment),
         group = this.state.selectedGroup;
+    console.log(555, fragment)
     return (
       <div>
         <Card title={ `${ group.name || "组名称" } ${ isEmpty(models) ? null : `${ models.length }个模板` }` } extra={ <span><a onClick={ this.showCreateCaseModal.bind(this) }><Icon type="plus-circle-o" /> 创建用例</a>&nbsp;&nbsp;&nbsp;&nbsp;<a onClick={ this.showCreateCaseModal.bind(this) }><Icon type="edit" /> 编辑</a>&nbsp;&nbsp;&nbsp;&nbsp;<Popconfirm title="您确定要删除此记录？" placement="bottom" onConfirm={ this.deleteGroup.bind(this) }><a><Icon type="cross-circle-o" /> 删除</a></Popconfirm></span> } className="panel">
@@ -70,8 +80,8 @@ export default class extends React.Component {
               {
                 isEmpty(models) ? <Spin done text="您还没有选择组，或者所选组暂无数据~" /> : models.map((v, i) => {
                   return (
-                    <li key={ i }>
-                      <Checkbox onChange={ this.changeGroupSelected.bind(this) }>
+                    <li key={ i } onClick={ this.selectedModel.bind(this, v) }>
+                      <Checkbox onChange={ this.checkedModel.bind(this) }>
                         <p className="link">{ v.name }</p>
                         <p className="time"><Icon type="clock-circle-o" /> { moment(v.updated_at).format("YYYY-MM-DD HH:mm:ss") } &nbsp;&nbsp;&nbsp;&nbsp;<Icon type="user" /> JSANN</p>
                       </Checkbox>
@@ -81,49 +91,60 @@ export default class extends React.Component {
               }
               </ul>
             </Col>
-            <Col span={14} className="group-item">
-              <div className="group-item-title clearfix">
-                <h4>das</h4>
-                <Button size="small">json</Button>
-              </div>
-              <div className="group-result clearfix">
-                <span className="group-result-info">预期结果：以下报错均出现</span>
-                <span className="group-result-control">
-                  <a onClick={ this.showCreateCaseModal.bind(this) }><Icon type="edit" /> 编辑</a>&nbsp;&nbsp;&nbsp;&nbsp;
-                  <Popconfirm title="您确定要删除此记录？" placement="bottom" onConfirm={ this.confirm.bind(this) }>
-                    <a><Icon type="cross-circle-o" /> 删除</a>
-                  </Popconfirm>
-                </span>
-              </div>
-              <ul className="action-line">
-                <li className="clearfix">
-                  <span className="index">1.</span>
-                  <span className="action-content-wrap">
-                    <span className="action-content">
-                      <div><span className="address"><Icon type="environment-o" /> //*[@id='button']</span> &nbsp;&nbsp;&nbsp;&nbsp; <span className="action"><Icon type="setting" /> click</span></div>
-                      <div><Button size="small">Comment</Button></div>
+            {
+              isEmpty(model) ? <Spin done text="您还没有选择模板，或者所选模板暂无数据~" /> : (
+                <Col span={ 14 } className="group-item">
+                  <div className="group-item-title clearfix">
+                    <h4>{ model.name }</h4>
+                    <Button size="small">代码</Button>
+                  </div>
+                  <div className="group-result clearfix">
+                    <span className="group-result-info">预期结果：以下报错均出现</span>
+                    <span className="group-result-control">
+                      <a onClick={ this.showCreateCaseModal.bind(this) }><Icon type="edit" /> 编辑</a>&nbsp;&nbsp;&nbsp;&nbsp;
+                      <Popconfirm title="您确定要删除此记录？" placement="bottom" onConfirm={ this.confirm.bind(this) }>
+                        <a><Icon type="cross-circle-o" /> 删除</a>
+                      </Popconfirm>
                     </span>
-                  </span>
-                </li>
-                <li className="clearfix">
-                  <span className="index">2.</span>
-                  <span className="action-content-wrap">
-                    <span className="action-content">
-                      <div><span className="address"><Icon type="environment-o" /> //*[@id='button']</span> &nbsp;&nbsp;&nbsp;&nbsp; <span className="action"><Icon type="setting" /> click</span></div>
-                      <div className="group-result small error clearfix">
-                        <span className="group-result-info">预期结果：以下报错均出现</span>
-                        <span className="group-result-control">
-                          <a onClick={ this.showCreateCaseModal.bind(this) }><Icon type="edit" /> 编辑</a>&nbsp;&nbsp;&nbsp;&nbsp;
-                          <Popconfirm title="您确定要删除此记录？" placement="bottom" onConfirm={ this.confirm.bind(this) }>
-                            <a><Icon type="cross-circle-o" /> 删除</a>
-                          </Popconfirm>
-                        </span>
-                      </div>
-                    </span>
-                  </span>
-                </li>
-              </ul>
-            </Col>
+                  </div>
+                  <ul className="action-line">
+                  {
+                    fragment.map(v => {
+                      return v.tArray.map((v, i) => {
+                        return (
+                          <li className="clearfix" key={ i }>
+                            <span className="index">{ i + 1 }.</span>
+                            <span className="action-content-wrap">
+                              <span className="action-content">
+                                <div>
+                                  <span className="address" title={ `xPath: ${ v.xPath }` }><Icon type="environment-o" /> { v.xPath }</span>
+                                  { v.className ? <span className="action" title={ `Class Name: ${ v.className }` }><Icon type="tag-o" /> { v.className }</span> : null }
+                                  { v.id ? <span className="action" title={ `ID: ${ v.id }` }><Icon type="tags-o" /> { v.id }</span> : null }
+                                  { v.name ? <span className="action" title={ `Name: ${ v.name }` }><Icon type="eye-o" /> { v.name }</span> : null }
+                                  { v.type ? <span className="action" title={ `Type: ${ v.type }` }><Icon type="file-unknown" /> { v.type }</span> : null }
+                                  { v.tagName ? <span className="action" title={ `Tag Name: ${ v.tagName }` }><Icon type="setting" /> { v.tagName }</span> : null }
+                                  { v.value ? <span className="address" title={ `Value: ${ v.type == "password" ? v.value.replace(/./g, "*") : v.value }` }><Icon type="book" /> { v.type == "password" ? v.value.replace(/./g, "*") : v.value }</span> : null }
+                                </div>
+                                <div><Button size="small">Comment</Button></div>
+                                <div className="group-result small error clearfix">
+                                  <span className="group-result-info">预期结果：以下报错均出现</span>
+                                  <span className="group-result-control">
+                                    <a onClick={ this.showCreateCaseModal.bind(this) }><Icon type="edit" /> 编辑</a><Popconfirm title="您确定要删除此记录？" placement="bottom" onConfirm={ this.confirm.bind(this) }>
+                                      <a><Icon type="cross-circle-o" /> 删除</a>
+                                    </Popconfirm>
+                                  </span>
+                                </div>
+                              </span>
+                            </span>
+                          </li>
+                        )
+                      })
+                    })
+                  }
+                  </ul>
+                </Col>
+              )
+            }
           </Row>
         </Card>
         <CreateCaseModal visible={ this.state.createCaseModalVisible } onSubmit={ this.createCaseModalSubmit.bind(this) } onClose={ this.closeCreatesModal.bind(this) } />
