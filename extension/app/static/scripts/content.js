966f0a17fde5f1dcf79977e50d6e118cf7cf45a7
MichaelZhao <MichaelZhaoZero@gmail.com>
2017-02-21 15:12:55 +0800

    conan form

--- extension/app/static/scripts/content.js
+++ extension/app/static/scripts/content.js
@@ -1,3 +1,6 @@
+// $("#kw").click(function(){
+//   alert("alan");
+// });
 chrome.storage.local.get('conan', function(result){
     var existWhite = false;
     if(result.conan.whiteLists.length != 0){
@@ -37,6 +40,8 @@ chrome.storage.local.get('conan', function(result){
             $('.tester_theme_popover_mask').fadeOut(100);
             $('#tester_show_element_view').slideUp(200);
         });
+
+        new ConanForm().initConanForm();
 
         // 监听元素事件
         $(document).mousedown(function(e){
@@ -394,3 +399,234 @@ function __local_domain(urlPath){
   var domain = oldDomainP.substring(0, oldDomainP.length-1);
   return domain;
 }
+
+/** 
+ * Conan Form
+ * 记录
+ * 记录页面input输入数据，并以列表方式展示。
+ * 依据xPath来定位页面元素
+ * 依据页面url&页面加载时间点作为form表单归集索引
+ * 回写
+ * 数据回写检索页面所有input元素
+ * 比对xPath 对应的form数据回写到页面元素
+ */
+__select_status = false;
+__indexDate = new Date().getTime();
+function ConanForm(){
+  this.initConanForm = function(){
+    $("input").focus(function(e){
+      var event_obj = $(e.target);
+      var form_obj = __pml_form_obj(event_obj);
+
+      // 获取extension资源
+      var imagePath = chrome.extension.getURL("assest/images/logo.png");
+      var new_obj = $("<div style='cursor:pointer;background-size:16px 16px;width:16px;height:16px;position:absolute;z-index:1000;background-image:url(\""+imagePath+"\"');'></div>")
+      
+      //获取相对定位及event_obj高度
+      var absolute_top = event_obj.height() + Number(event_obj.css('padding-top').replace("px","")) + Number(event_obj.css('padding-bottom').replace("px",""));
+      //设置相对定位
+      new_obj.css("top", event_obj.position().top + (absolute_top - new_obj.height())/2);
+      new_obj.css("left", event_obj.position().left + event_obj.width() + Number(event_obj.css('padding-left').replace("px","")) - new_obj.width());
+
+      event_obj.after(new_obj);
+
+      //数据归集
+      chrome.storage.local.get('conanForm', function(result){
+        var html = '<ul class="list-group"'+' indexXPath="'+form_obj["xPath"]+'">';
+        for(index in result.conanForm.formList){
+          var index_form = result.conanForm.formList[index];
+          if(form_obj["formIndex"] == index_form["formIndex"]){
+            for(f_index in index_form["formArray"]){
+              if(form_obj["xPath"] == index_form["formArray"][f_index]["xPath"]){
+                html+='<li indexDate="'+index_form["formArray"][f_index]["indexDate"]+'" formDataVal='+index_form["formArray"][f_index]["value"]+' class="list-group-item">'
+                    + index_form["formArray"][f_index]["value"]+'</li>'; 
+              }
+            }
+          }
+        }
+        html+='</ul>';
+
+        __initPopover(new_obj , html, form_obj);
+      });
+    });
+
+    $("input").blur(function(e){
+        var event_obj = $(e.target);
+
+        __save_form_obj(event_obj);
+    });
+  }
+
+  /**
+   * 获取表单元素
+   */
+  function __pml_form_obj(event_obj){
+    // 元素信息
+    var obj_val = {};
+    for (var pro in property){
+      if(event_obj[0][property[pro]]){
+        obj_val[property[pro]] = event_obj[0][property[pro]];
+      }
+    }
+    // 元素xPath定位数据
+    obj_val["xPath"] = event_obj.localXpath().xpath;
+
+    // 元素是否是form输入元素({"INPUT" : ["text", "password"]}, {"SELECT": []})
+    obj_val["isFormEl"] = is_form_element(obj_val);
+
+    // 元素记录时间
+    obj_val["inDate"] = new Date();
+
+    // form时间索引
+    obj_val["indexDate"] = __indexDate;
+
+    //截取不包含请求参数部分url
+    var end_index = obj_val["baseURI"].indexOf("?") != -1 ? obj_val["baseURI"].indexOf("?") : obj_val["baseURI"].length;
+    obj_val["formIndex"] = obj_val["baseURI"].substring(0 , end_index);
+
+    return obj_val;
+  }
+
+  /**
+   * 表单数据记录
+   */
+  function __save_form_obj(event_obj){
+    var obj_val = pml_form_obj(event_obj);
+
+    //是否是输入元素
+    if(!obj_val["isFormEl"]){
+      return;
+    }
+
+    //空数据
+    if(!obj_val["value"] || obj_val["value"] == ""){
+      return;
+    }
+
+    chrome.storage.local.get('conanForm', function(result){
+        if(!result.conanForm){
+          result.conanForm = {};
+        }
+
+        // 测试同步服务端
+        if(!result.conanForm.formList){
+          result.conanForm.formList = [];
+        }
+
+        var indexForm = null;
+        for(index in result.conanForm.formList){
+          if(result.conanForm.formList[index]["formIndex"] == obj_val["formIndex"] 
+            && result.conanForm.formList[index]["indexDate"] == __indexDate){
+            indexForm = result.conanForm.formList[index];
+            break;
+          }
+        }
+
+        if(indexForm){
+          //相同数据去重
+          if(obj_val["isFormEl"]){
+            for(fIndex in indexForm["formArray"]){
+              if(obj_val["xPath"] == indexForm["formArray"][fIndex]["xPath"] 
+                && obj_val["value"] == indexForm["formArray"][fIndex]["value"]){
+                return;
+              }
+            }
+          }
+          indexForm["formArray"].push(obj_val);
+        } else {
+          indexForm = {"formIndex" : obj_val["formIndex"], "indexDate" : __indexDate, "formArray": []};
+          indexForm["formArray"].push(obj_val);
+          result.conanForm.formList.push(indexForm);
+        }
+
+        chrome.storage.local.set(result);
+    });
+  }
+
+  function __initPopover(ent_obj, data_html, form_obj){   
+    var settings = {
+      trigger:'click',
+      title:'Coana Form',
+      width:200,
+      height:323,          
+      multi:true,
+      closeable:true,
+      style:'',
+      padding:true,
+      backdrop:true,
+      content: data_html,
+      onShow: function(e){
+        var a_objs = e.find("li");
+
+        __select_status = false;
+        var form_index = form_obj["formIndex"];
+        a_objs.map(function(){
+          var index_date_id = $(this).attr("indexDate");
+          $(this).click(function(){
+            __select_status = true;
+            ent_obj.webuiPopover("hide");
+          });
+
+          $(this).on("mouseover", function(){  
+            var data_val = $(this).attr("formDataVal");
+            var index_date = $(this).attr("indexDate");
+            var eve_xpath = form_obj["xPath"];
+
+            chrome.storage.local.get('conanForm', function(result){
+              var form_vals = [];
+              for(index in result.conanForm.formList){
+                var index_form = result.conanForm.formList[index];
+                if(form_obj["formIndex"] == index_form["formIndex"]){
+                  for(f_index in index_form["formArray"]){
+                    if(index_date == index_form["formArray"][f_index]["indexDate"] &&
+                      eve_xpath != index_form["formArray"][f_index]["xPath"]){
+                      form_vals.push(index_form["formArray"][f_index]);
+                    } else if(index_date == index_form["formArray"][f_index]["indexDate"] &&
+                      eve_xpath == index_form["formArray"][f_index]["xPath"] && 
+                      data_val == index_form["formArray"][f_index]["value"]){
+                      form_vals.push(index_form["formArray"][f_index]);
+                    }
+                  }
+                }
+              }
+
+              $("input").map(function(){
+                var input_obj = $(this);
+                var exist = false;
+                for(index in form_vals){
+                  if(input_obj.localXpath().xpath == form_vals[index]["xPath"]){
+                    exist = true;
+                    break;
+                  }
+                }
+
+                if(exist){
+                  input_obj.css("background-color","#b6d7a8");
+                  input_obj.val(form_vals[index]["value"]);
+                }else{
+                  input_obj.css("background-color","white");
+                  input_obj.val("");
+                }
+              });
+            });
+          });
+        });
+      },
+      onHide: function(e){
+        //未选择 则去除全部的选择
+        if(!__select_status){
+          $("input").map(function(){
+            var input_obj = $(this);
+            input_obj.css("background-color","white");
+            input_obj.val("");
+          });
+        }
+
+        ent_obj.remove();
+      }
+    };
+
+    ent_obj.webuiPopover('destroy').webuiPopover(settings);
+  }
+}
+
